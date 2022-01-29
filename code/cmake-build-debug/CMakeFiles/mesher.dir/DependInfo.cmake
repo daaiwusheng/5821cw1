7794db1dac85f2c1dd7c683fcef03a470eea9de8
YuWang(Steven) <daaiwusheng003@gmail.com>
2022-01-29 20:44:54 +0000

    配置文件

---
+++ code/cmake-build-debug/CMakeFiles/mesher.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5821/作业1/我自己的作业1/submit/code/Cartesian3.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5821/作业1/我自己的作业1/submit/code/cmake-build-debug/CMakeFiles/mesher.dir/Cartesian3.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5821/作业1/我自己的作业1/submit/code/face2faceindex.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5821/作业1/我自己的作业1/submit/code/cmake-build-debug/CMakeFiles/mesher.dir/face2faceindex.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5821/作业1/我自己的作业1/submit/code/faceindex2directededge.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5821/作业1/我自己的作业1/submit/code/cmake-build-debug/CMakeFiles/mesher.dir/faceindex2directededge.cpp.o"
+  "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5821/作业1/我自己的作业1/submit/code/main.cpp" "/Users/wangyu/Desktop/利兹上课资料/9月份学期/图形学两门课/5821/作业1/我自己的作业1/submit/code/cmake-build-debug/CMakeFiles/mesher.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/cmake-build-debug/CMakeFiles/mesher.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mesher.dir/Cartesian3.cpp.o"
+  "CMakeFiles/mesher.dir/face2faceindex.cpp.o"
+  "CMakeFiles/mesher.dir/faceindex2directededge.cpp.o"
+  "CMakeFiles/mesher.dir/main.cpp.o"
+  "mesher"
+  "mesher.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mesher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
